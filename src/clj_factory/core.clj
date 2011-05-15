1d8a12c2ffd060957aa97e750f40143e2454b29f
Daniel E. Renfer <duck@kronkltd.net>
2011-05-15 11:41:08 -0400

    initial commit

---
+++ src/clj_factory/core.clj
@@ -0,0 +1,1 @@
+(ns clj-factory.core)
---
+++ test/clj_factory/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-factory.test.core
+  (:use [clj-factory.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
