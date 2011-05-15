7518883c6e62037e2b6ab1fc6d16969b8f03fa6f
Daniel E. Renfer <duck@kronkltd.net>
2011-05-15 11:52:45 -0400

    factory

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject clj-factory "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "Library for producing test data from factories"
+  :dependencies [[org.clojure/clojure "1.3.0-alpha7"]]
+  :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]])
