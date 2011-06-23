f9205c47c202447997d73f05e2b80dfefae9729e
Daniel E. Renfer <duck@kronkltd.net>
2011-06-23 11:43:54 -0400

    remove lazytest

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-factory "1.0.0-SNAPSHOT"
   :description "Library for producing test data from factories"
   :dependencies [[org.clojure/clojure "1.3.0-alpha7"]]
-  :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
-  :repositories {"lazytest" "http://stuartsierra.com/maven2"}
+  ;; :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
+  ;; :repositories {"lazytest" "http://stuartsierra.com/maven2"}
   )
--- test/clj_factory/test/core.clj
+++ test/clj_factory/test/core.clj
@@ -1,32 +1,32 @@
-(ns clj-factory.test.core
-  (:use [clj-factory.core]
-        [lazytest.describe :only (describe it do-it testing given)]
-        [lazytest.expect :only (expect)]))
+;; (ns clj-factory.test.core
+;;   (:use [clj-factory.core]
+;;         [lazytest.describe :only (describe it do-it testing given)]
+;;         [lazytest.expect :only (expect)]))
 
-(describe get-counter
-  (testing "when the type has been defined"
-    (do-it "should return a number"
-      (let [type :foo]
-        (reset-counter! type)
-        (let [response (get-counter type)]
-          (expect (= 0 response)))))
-    (testing "when it has been incremented"
-      (do-it "should return a larger number"
-        (let [type :foo]
-          (reset-counter! type)
-          (let [response1 (get-counter type)]
-            (inc-counter! type)
-            (let [response2 (get-counter type)]
-              (expect (< response1 response2)))))))))
+;; (describe get-counter
+;;   (testing "when the type has been defined"
+;;     (do-it "should return a number"
+;;       (let [type :foo]
+;;         (reset-counter! type)
+;;         (let [response (get-counter type)]
+;;           (expect (= 0 response)))))
+;;     (testing "when it has been incremented"
+;;       (do-it "should return a larger number"
+;;         (let [type :foo]
+;;           (reset-counter! type)
+;;           (let [response1 (get-counter type)]
+;;             (inc-counter! type)
+;;             (let [response2 (get-counter type)]
+;;               (expect (< response1 response2)))))))))
 
-(describe inc-counter!)
+;; (describe inc-counter!)
 
-(describe next-counter!)
+;; (describe next-counter!)
 
-(describe fseq)
+;; (describe fseq)
 
-(describe factory)
+;; (describe factory)
 
-(describe deffactory)
+;; (describe deffactory)
 
-(describe defseq)
+;; (describe defseq)
