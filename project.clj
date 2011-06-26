834c7114cab8a82ac83f16df10a5379cadc8b19b
Daniel E. Renfer <duck@kronkltd.net>
2011-06-26 09:17:45 -0400

    mvn build

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject clj-factory "1.0.0-SNAPSHOT"
-  :description "Library for producing test data from factories"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha7"]
-                 [org.clojure.contrib/core "1.3.0-alpha4"]]
-  ;; :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
-  ;; :repositories {"lazytest" "http://stuartsierra.com/maven2"}
-  )
