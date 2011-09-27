6264cdcadc095d51e7db376214019280a4246bec
Daniel E. Renfer <duck@kronkltd.net>
2011-09-27 12:31:27 -0400

    add documentation

--- src/clj_factory/core.clj
+++ src/clj_factory/core.clj
@@ -1,8 +1,11 @@
 (ns clj-factory.core)
 
+;; # Counter functions
+
 (def ^:dynamic *counters* (ref {}))
 
 (defn set-counter!
+  "Set the counter for the supplied key to the new value"
   [type value]
   (get
    (dosync
@@ -12,14 +15,17 @@
    type))
 
 (defn reset-counter!
+  "Reset the counter for the supplied key to 0"
   [type]
   (set-counter! type 0))
 
 (defn get-counter
+  "Get the counter for the supplied key"
   [type]
   (get @*counters* type 0))
 
 (defn inc-counter!
+  "Increment the counter for the supplied key by 1"
   [type]
   (dosync
    (alter
@@ -28,8 +34,11 @@
       (assoc m type (inc (get m type 0)))))))
 
 (defn next-counter!
+  "Increment and return the next value for the supplied key"
   [type]
   (get (inc-counter! type) type))
+
+;; # Multimethods
 
 (defmulti fseq (fn [type & _] type))
 
