5c08c29996c036da93b1d9792f14eb040337cc67
Daniel E. Renfer <duck@kronkltd.net>
2012-02-08 18:47:23 -0500

    update midje dependency

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Library for producing test data from factories"
   :url "https://github.com/duck1123/clj-factory"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.3-alpha5" :exclusions [org.clojure/clojure]]]
+  :dev-dependencies [[midje "1.3.0" :exclusions [org.clojure/clojure]]]
   :warn-on-reflection false)
