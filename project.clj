2890f610be847c0777ca965d0a2c26388c020a18
Daniel E. Renfer <duck@kronkltd.net>
2012-08-07 20:49:54 -0400

    bump version to 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-factory "0.2.0"
+(defproject clj-factory "0.2.1-SNAPSHOT"
   :description "Library for producing test data from factories"
   :url "https://github.com/duck1123/clj-factory"
   :dependencies [[org.clojure/clojure "1.3.0"]]
