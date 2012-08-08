36a84029779b15e34de379a8f407b0a55c931ce1
Daniel E. Renfer <duck@kronkltd.net>
2012-08-07 22:28:20 -0400

    upgrade to lein 2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject clj-factory "0.2.1-SNAPSHOT"
   :description "Library for producing test data from factories"
   :url "https://github.com/duck1123/clj-factory"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.3.0" :exclusions [org.clojure/clojure]]]
+  :author "Daniel E. Renfer <duck@kronkltd.net>"
+  :min-lein-version "2.0.0"
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev
+             {:dependencies [[midje "1.3.0"]]}}
   :warn-on-reflection false)
