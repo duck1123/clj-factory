7f470abaaea867b671c8618f41390320ce93d057
Daniel E. Renfer <duck@kronkltd.net>
2012-10-08 22:04:27 -0400

    release 0.2.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-factory "0.2.1-SNAPSHOT"
+(defproject clj-factory "0.2.1"
   :description "Library for producing test data from factories"
   :url "https://github.com/duck1123/clj-factory"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
   :min-lein-version "2.0.0"
   :plugins [[codox "0.6.1"]
-            [lein-midje "2.0.0-SNAPSHOT"]]
+            [lein-midje "2.0.0-20120327.004212-2"]]
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev
              {:dependencies [[midje "1.4.0"]]}})
