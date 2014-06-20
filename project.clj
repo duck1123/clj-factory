ed8f6936866596039ee256728ad7d83c5ca1cafe
Daniel E. Renfer <duck@kronkltd.net>
2014-06-20 17:52:05 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/duck1123/clj-factory"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
   :min-lein-version "2.0.0"
-  :plugins [[codox "0.6.1"]
-            [lein-midje "3.0-alpha4"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[codox "0.6.7"]
+            [lein-midje "3.1.3"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev
-             {:dependencies [[midje "1.5-alpha10"]]}})
+             {:dependencies [[midje "1.6.3"]]}})
